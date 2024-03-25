ab856d7d5fcc36292f434a17b1a7f584723ecd91
GMurira <nguyaimurira@gmail.com>
2024-03-25 09:58:43 +0300

    Working on home Screen:

--- app/src/main/java/com/example/insatmark/ui/theme/screens/Commons.kt
+++ app/src/main/java/com/example/insatmark/ui/theme/screens/Commons.kt
@@ -1,17 +1,46 @@
 package com.example.insatmark.ui.theme.screens
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.insatmark.R
 
 /**
  * Contains the app common components
  */
 @Composable
-fun InputCommon(){
+fun MyTextField(labelValue: Int){
+    val textValue by remember{ mutableStateOf("")}
     OutlinedTextField(
-        value = "",
+        value = textValue,
         onValueChange = {},
-        singleLine = true,
+        // label = labelValue,
 
     )
+}
+
+/**
+ * Terms and Conditions
+ */
+@Composable
+fun TermsAndConditions(){
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(text = stringResource(id = R.string.TandCS))
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun TermsAndConditionsPreview(){
+    TermsAndConditions()
 }
